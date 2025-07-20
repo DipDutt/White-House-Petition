c8e13f437fc3ebaa4f95a171c63bf34e460854a5
Dip Dutt <emdutt456@gmail.com>
2025-07-20 16:21:59 +0600

    add url sesssion code into DisPatchQueque.global.asyn section so that data fectching happen  in the background thread.

--- WhiteHousePetition/WhiteHousePetition/Model/NetworkManager.swift
+++ WhiteHousePetition/WhiteHousePetition/Model/NetworkManager.swift
@@ -8,23 +8,24 @@
 import Foundation
 import UIKit
 
- // MARK: - Create downloadData method
+// MARK: - Create downloadData method
 func downloadData(fromURL url: URL, completionHandler: @escaping (_ data: Data?) -> ()) {
-    
-    URLSession.shared.dataTask(with: url) { (data,response,error,) in
-        guard
-            let data = data,
-            error == nil,
-            let response = response as? HTTPURLResponse,
-            response.statusCode >= 200 && response.statusCode < 400 else {
-            print("Error downloading data.")
-            //completionHandler(nil)
-            return
+    DispatchQueue.global(qos: .userInitiated).async {
+        URLSession.shared.dataTask(with: url) { (data,response,error,) in
+            guard
+                let data = data,
+                error == nil,
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 400 else {
+                print("Error downloading data.")
+                //completionHandler(nil)
+                return
+            }
+            completionHandler(data)
+            
         }
-       completionHandler(data)
-        
+        .resume()
     }
-    .resume()
 }
 
 
